eba061034d1d2d6bbdf52a3a85d2969dbadf6cae
LodewijkXCII <loek@zweersdesign.com>
2021-05-02 15:42:06 +0200

    fixing scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['~/assets/css/config.scss'],
+  css: [{ src: '~/assets/css/config.scss', lang: 'scss' }],
   /*
    ** Plugins to load before mounting the App
    */
