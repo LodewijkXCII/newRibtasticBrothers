659fb0865dc2617144ae1f3dd2616e7da95657c2
LodewijkXCII <loek@zweersdesign.com>
2022-08-18 23:14:02 +0200

    new stuff

---
+++ src/components/PartnerCarousel.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>PartnerCarousel</h2>
+  <div v-for="(partner, index) in partners" :key="partner.id">
+    <h1>{{ partner.bedrijf }}</h1>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      partners: [],
+    };
+  },
+  async created() {
+    const { data } = await axios.get(
+      "https://admin.ribtasticbrothers.nl/catering-partners?_sort=bedrijf"
+    );
+
+    this.partners = data;
+  },
+};
+</script>
+<style lang=""></style>
