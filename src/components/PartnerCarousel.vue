965f8fd4fbf9cb7ae3fb8403755607ae09b6d419
LodewijkXCII <loek@zweersdesign.com>
2023-01-18 21:33:04 +0100

    update for dev

--- src/components/PartnerCarousel.vue
+++ src/components/PartnerCarousel.vue
@@ -1,11 +1,20 @@
 <template>
   <h2>PartnerCarousel</h2>
-  <div v-for="(partner, index) in partners" :key="partner.id">
-    <h1>{{ partner.bedrijf }}</h1>
+  <div class="carousel--container">
+    <div
+      v-for="(partner, index) in partners"
+      :key="partner.id"
+      class="carousel--item"
+    >
+      <img :src="partner.logo.url" alt="" />
+      <span>{{ partner.logo.url }}</span>
+    </div>
   </div>
 </template>
+
 <script>
 import axios from "axios";
+
 export default {
   data() {
     return {
@@ -19,6 +28,30 @@ export default {
 
     this.partners = data;
   },
+  async mounted() {
+    $(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+});
+  }
 };
 </script>
-<style lang=""></style>
+
+<style lang="scss">
+.carousel {
+  &--container {
+    display: flex;
+  }
+
+  &--item {
+    width: 500px;
+    background: lightgray;
+    padding: 0.75rem;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
