09cbf0fa256da2e51904105c304cef7d87980ba7
LodewijkXCII <loek@zweersdesign.com>
2023-02-27 16:00:13 +0100

    updating

--- src/components/PartnerCarousel.vue
+++ src/components/PartnerCarousel.vue
@@ -1,51 +1,115 @@
 <template>
   <h2>PartnerCarousel</h2>
   <div class="carousel--container">
-    <div
-      v-for="(partner, index) in partners"
-      :key="partner.id"
-      class="carousel--item"
+    <!-- <VueSlickCarousel
+      v-bind="settings"
+      v-if="$slots.default && $slots.default.length"
     >
-      <img :src="partner.logo.url" alt="" />
-      <span>{{ partner.logo.url }}</span>
-    </div>
+      <div
+        v-for="(partner, index) in partners"
+        :key="partner.id"
+        class="carousel--item"
+      >
+        
+      </div>
+    </VueSlickCarousel> -->
+    <swiper
+      :slidesPerView="1"
+      :spaceBetween="30"
+      :loop="true"
+      :pagination="{
+        clickable: true,
+      }"
+      :navigation="true"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+      <swiper-slide>Slide 6</swiper-slide>
+      <!-- <swiper-slide
+        v-for="(partner, index) in partners"
+        :key="partner.id"
+        class="carousel--item"
+        ><span>{{ partner.logo.url }}</span></swiper-slide
+      > -->
+    </swiper>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 
+// import VueSlickCarousel from "vue-slick-carousel";
+
+// import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Pagination, Navigation } from "swiper/vue";
+
+// Import Swiper styles
+
 export default {
-  data() {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+
+  setup() {
     return {
-      partners: [],
+      modules: [Pagination, Navigation],
     };
   },
-  async created() {
-    const { data } = await axios.get(
-      "https://admin.ribtasticbrothers.nl/catering-partners?_sort=bedrijf"
-    );
 
-    this.partners = data;
-  },
-  async mounted() {},
+  // data() {
+  //   return {
+  //     partners: [],
+  //     onSwiper,
+  //     onSlideChange,
+  //   };
+  // },
+  // async created() {
+  //   const { data } = await axios.get(
+  //     "https://admin.ribtasticbrothers.nl/catering-partners?_sort=bedrijf"
+  //   );
+  //   console.log(data);
+  //   this.partners = data;
+
+  //   const swiper = new Swiper();
+  // },
+  // async mounted() {
+  //   const onSwiper = (swiper) => {
+  //     console.log(swiper);
+  //   };
+  //   const onSlideChange = () => {
+  //     console.log("slide change");
+  //   };
+  // },
 };
 </script>
 
 <style lang="scss">
-.carousel {
-  &--container {
-    display: flex;
-  }
+// .carousel {
+//   &--container {
+//     display: flex;
+//   }
 
-  &--item {
-    width: 500px;
-    background: lightgray;
-    padding: 0.75rem;
+//   &--item {
+//     width: 500px;
+//     background: lightgray;
+//     padding: 0.75rem;
 
-    img {
-      width: 100%;
-    }
-  }
-}
+//     img {
+//       width: 100%;
+//     }
+//   }
+// }
 </style>
