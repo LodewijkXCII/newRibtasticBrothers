bb5dd815f1213d208382e8b5259799f7a17ce76c
LodewijkXCII <loek@zweersdesign.com>
2023-03-14 14:11:32 +0100

    updating navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config";
 
-export default {
+export default defineConfig({
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
+  build: {
     // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true, // Generate sitemap (set to "false" to disable)
   },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-  },
-  renderers: ["@astrojs/renderer-vue"],
+
+  server: { port: 1234, host: true },
+
   integrations: [vue()],
-};
+});
